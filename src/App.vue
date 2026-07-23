0d1d69e36c0887a4345a51cfc810109fe01d5387
Camille Florent Djumtchemeu <camillemogomye@gmail.com>
2026-07-23 11:43:39 +0100

    Initialisation des variables globales et nettoyage du fichier app.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-container">
+    <!-- Nos futurs composants viendront ici -->
+    <h1 style="color: var(--color-gold); text-align: center; margin-top: 50px">
+      Portfolio de Camille - Prêt pour l'intégration
+    </h1>
+  </div>
+</template>
+
+<script setup>
+// C'est ici que nous importerons nos composants plus tard
+</script>
+
+<style>
+/* Style global à l'application si nécessaire */
+</style>
